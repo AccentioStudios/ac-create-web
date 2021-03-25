581fb906c5507af37df26b28e695550d0a11515b
Alvaro Gabriel Gomez <alvarogabrielgomez@gmail.com>
2021-03-24 23:57:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+if (major < 10) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'AC-Create App requires Node 10 or higher. \n' +
+      'Please update your version of Node.'
+  );
+  process.exit(1);
+}
+
+const { init } = require('./acCreateApp');
+
+init();
